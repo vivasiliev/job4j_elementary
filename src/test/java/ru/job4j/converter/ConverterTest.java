a73465e5d2356a1e93a9d5931d5d2a62b8316889
Vitaliy <vivasiliev.a@yandex.ru>
2023-06-09 18:22:32 +0300

    Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,8 +1,7 @@
 package ru.job4j.converter;
 
-import org.junit.Test;
 import org.junit.Assert;
-
+import org.junit.Test;
 public class ConverterTest {
     @Test
     public void whenConvert140RblThen2Euro() {
