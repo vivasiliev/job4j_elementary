11629dfd6ec68bffd19f3ee0069de1995327731f
Vitaliy <vivasiliev.a@yandex.ru>
2023-06-11 22:18:13 +0300

    Глупый бот [#31323 #422307]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,10 +4,8 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
             rsl =  "Hi, SmartAss.";
-        } else if ("Bye.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+        } else if ("Bye.".equals(question)) {
             rsl = "See you later.";
         }
         return rsl;
