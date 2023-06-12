86789bf9a233c60736ffc49c939dbfcaa0b94af3
Vitaliy <vivasiliev.a@yandex.ru>
2023-06-12 09:59:05 +0300

    Множественное логическое выражение И [#9461]

---
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(12, 13, 15));
+    }
+}
