64c2511f7b1f016805b193cf50bb0ef0d1f824ae
Vitaliy <vivasiliev.a@yandex.ru>
2023-06-14 22:25:49 +0300

    Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * percent / 100;
+            amount -= salary;
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(year(100, 120, 50));
+    }
+}
