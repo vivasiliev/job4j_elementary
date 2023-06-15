8d3a939c9a60b0dfdfe5dbd222f68fc7e8a35f05
Vitaliy <vivasiliev@icloud.com>
2023-06-15 22:44:58 +0300

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println(ages.length);
-        System.out.println(surnames.length);
-        System.out.println(prices.length);
+        String[] name = new String[4];
+        name[0] = "Petr Arsentev";
+        name[1] = "Ivanov Ivan";
+        name[2] = "Pavel Pavlovich";
+        name[3] = "Vasiliy Petrovich";
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+        System.out.println(name[3]);
     }
 }
