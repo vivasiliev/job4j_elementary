7cb161a97e8323087d633de295ff780931038303
Vitaliy <vivasiliev@icloud.com>
2023-06-19 21:29:40 +0300

    Сортировка выборкой. [#156320]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, index, i);
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(sort(data)));
+    }
+}
